45784c822f4d9ca2fcc5d2c791fe40a242cc73f8
jmorataya2019041 <jmorataya-2019041@kinal.edu.gt>
2021-10-17 22:30:32 -0600

    Se prueba la pestaña de prueba

--- pruebaSpinner/src/app/app-routing.module.ts
+++ pruebaSpinner/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PruebaComponent } from './componentes/prueba/prueba.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'prueba',
+    component: PruebaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- pruebaSpinner/src/app/componentes/prueba/prueba.component.ts
+++ pruebaSpinner/src/app/componentes/prueba/prueba.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-prueba',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PruebaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title) {
+    this.titleService.setTitle("Pestaña de prueba")
+  }
 
   ngOnInit(): void {
   }
